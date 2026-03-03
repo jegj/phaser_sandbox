87448d71277e25b27d76a2dbbecbe78d96130e8a
Javier Galarza <jegj57@gmail.com>
2026-03-03 08:03:58 -0500

    feat: continue

--- planet_defense/src/game/scenes/game.ts
+++ planet_defense/src/game/scenes/game.ts
@@ -1,7 +1,10 @@
 import { Scene } from 'phaser';
 import { SCENE_KEYS } from '../common/scenes.keys';
+import { ASSET_KEYS } from '../common/assets';
 
 export class Game extends Scene {
+  #planet: Phaser.GameObjects.Sprite;
+  #player: Phaser.GameObjects.Image;
   constructor() {
     super(
       {
@@ -11,6 +14,10 @@ export class Game extends Scene {
   }
 
   create() {
-    console.log('Game scene created');
+    this.add.sprite(0, 0, ASSET_KEYS.BACKGROUND_1).setOrigin(0).setScale(1, 1.25).play(ASSET_KEYS.BACKGROUND_1).setAlpha(0.5);
+    this.add.sprite(0, 0, ASSET_KEYS.BACKGROUND_2).setOrigin(0).setScale(1, 1.25).play(ASSET_KEYS.BACKGROUND_2).setAlpha(0.5);
+    this.add.sprite(0, 0, ASSET_KEYS.BACKGROUND_3).setOrigin(0).setScale(1, 1.25).play(ASSET_KEYS.BACKGROUND_3).setAlpha(0.5);
+    this.#planet = this.add.sprite(this.scale.width / 2, this.scale.height / 2, ASSET_KEYS.PLANET).setScale(1.5).play(ASSET_KEYS.PLANET);
+    this.#player = this.add.image(320, 180, ASSET_KEYS.SHIP);
   }
 }
--- planet_defense/src/game/scenes/preload.ts
+++ planet_defense/src/game/scenes/preload.ts
@@ -1,5 +1,6 @@
 import { Scene } from 'phaser';
 import { SCENE_KEYS } from '../common/scenes.keys';
+import { IMAGE_ASSETS, AUDIO_ASSETS, SPRITESHEET_ASSETS } from '../common/assets';
 
 export class Preload extends Scene {
   constructor() {
@@ -10,8 +11,35 @@ export class Preload extends Scene {
     );
   }
 
+  preload() {
+    IMAGE_ASSETS.forEach((asset) => {
+      this.load.image(asset.assetKey, asset.path);
+    });
+
+    AUDIO_ASSETS.forEach((asset) => {
+      this.load.audio(asset.assetKey, asset.path);
+    });
+
+    SPRITESHEET_ASSETS.forEach((asset) => {
+      this.load.spritesheet(asset.assetKey, asset.path, {
+        frameWidth: asset.frameWidth,
+        frameHeight: asset.frameHeight,
+      });
+    });
+  }
+
   create() {
-    console.log('Preload scene created');
+    SPRITESHEET_ASSETS.forEach((asset) => {
+      this.anims.create(
+        {
+          key: asset.assetKey,
+          frames: this.anims.generateFrameNumbers(asset.assetKey),
+          frameRate: asset.frameRate,
+          repeat: asset.repeat,
+        }
+      );
+    });
+
     this.scene.start(SCENE_KEYS.GAME_SCENE);
   }
 }

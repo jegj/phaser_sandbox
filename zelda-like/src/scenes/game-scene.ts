47134b1e1cab5aa4739da1e3cf1f6e468105bfde
Javier Galarza <jegj57@gmail.com>
2026-03-12 07:48:51 -0500

    feat: apply input to player

--- zelda-like/src/scenes/game-scene.ts
+++ zelda-like/src/scenes/game-scene.ts
@@ -1,10 +1,12 @@
 import * as Phaser from "phaser";
 import { ASSET_KEYS } from "../common/assets";
+import { KeyboardComponent } from "../components/input/keyboard-component";
 import { Player } from "../game-objects/player/player";
 import { SCENE_KEYS } from "./scene-keys";
 
 export class GameScene extends Phaser.Scene {
-  private player!: Phaser.Physics.Arcade.Sprite;
+  private player: Phaser.Physics.Arcade.Sprite;
+  private controls: KeyboardComponent;
   constructor() {
     super({
       key: SCENE_KEYS.GAME_SCENE,
@@ -12,6 +14,13 @@ export class GameScene extends Phaser.Scene {
   }
 
   public create(): void {
+    if (!this.input.keyboard) {
+      console.warn(
+        "Keyboard input is not available. Controls will not be initialized.",
+      );
+      return;
+    }
+    this.controls = new KeyboardComponent(this.input.keyboard);
     this.add
       .text(this.scale.width / 2, this.scale.height / 2, "Game Scene", {
         fontFamily: ASSET_KEYS.FONT_PRESS_START_2P,
@@ -22,6 +31,7 @@ export class GameScene extends Phaser.Scene {
       position: { x: this.scale.width / 2, y: this.scale.height / 2 },
       assetKey: ASSET_KEYS.PLAYER,
       frame: 0,
+      controls: this.controls,
     });
   }
 }
